b163db7c6658f078098eebf69f68b9a852d820b6
yiwenlong <wlong.yi@gmail.com>
2021-03-17 10:43:10 +0800

    Refactor Node class.

--- config/config.py
+++ config/config.py
@@ -28,20 +28,30 @@ class BaseConfigModel(dict):
 
 class Node(BaseConfigModel):
 
-    def __init__(self, **source):
+    def __init__(self, organization, **source):
         super().__init__()
         self.update(source)
+        self.MSPID = organization.MSPID
+        self.OrgName = organization.Name
+        self.Domain = "%s.%s" % (self.Name, organization.Domain)
+        self.Address = "%s:%s" % (self.Domain, self.ListenPort)
+        self.ListenAddress = "0.0.0.0:%s" % self.ListenPort
+        self.OperationsListenAddress = "127.0.0.1:%s" % self.OperationsListenPort
+        try:
+            self.ChaincodeListenAddress = "0.0.0.0:%s" % self.ChaincodeListenPort
+        except KeyError:
+            pass
 
-    def generate_peer_config(self, target_dir, core_yaml_template, mspid, gossip_bootstrap):
+    def generate_peer_config(self, target_dir, core_yaml_template, gossip_bootstrap):
         with open(core_yaml_template, 'r') as template:
             core_yaml_data = yaml.load(template, yaml.CLoader)
         # modify
-        core_yaml_data["peer"]["listenAddress"] = "0.0.0.0:%s" % self.ListenPort
-        core_yaml_data["peer"]["address"] = "0.0.0.0:%s" % self.ListenPort
-        core_yaml_data["peer"]["chaincodeListenAddress"] = "0.0.0.0:%s" % self.ChaincodeListenPort
+        core_yaml_data["peer"]["listenAddress"] = self.ListenAddress
+        core_yaml_data["peer"]["address"] = self.ListenAddress
+        core_yaml_data["peer"]["chaincodeListenAddress"] = self.ChaincodeListenAddress
         core_yaml_data["peer"]["gossip"]["bootstrap"] = gossip_bootstrap
-        core_yaml_data["peer"]["localMspId"] = mspid
-        core_yaml_data["operations"]["listenAddress"] = "127.0.0.1:%s" % self.OperationsListenPort
+        core_yaml_data["peer"]["localMspId"] = self.MSPID
+        core_yaml_data["operations"]["listenAddress"] = self.OperationsListenAddress
         # dump into target directory.
         with open(os.path.join(target_dir, "core.yaml"), 'w') as target_file:
             yaml.dump(core_yaml_data, target_file)
@@ -49,7 +59,7 @@ class Node(BaseConfigModel):
     def generate_orderer_config(self, target_dir):
         pass
 
-    def config_peer(self, base_dir, peer_binary, core_yaml_template, msp_dir, tls_dir, mspid, gossip_bootstrap):
+    def config_peer(self, base_dir, peer_binary, core_yaml_template, msp_dir, tls_dir, gossip_bootstrap):
         node_dir = os.path.join(base_dir, self.Name)
         if os.path.exists(node_dir):
             raise Exception("Target node directory already exist: %s" % node_dir)
@@ -57,8 +67,8 @@ class Node(BaseConfigModel):
         os.system("cp -r %s %s" % (msp_dir, os.path.join(node_dir, "msp")))
         os.system("cp -r %s %s" % (tls_dir, os.path.join(node_dir, "tls")))
         os.system("cp %s %s" % (peer_binary, node_dir))
-        self.generate_peer_config(node_dir, core_yaml_template, mspid, gossip_bootstrap)
-        daemon.config_daemon(node_dir, "easy-dev-%s-%s" % (mspid, self.Name), "peer node start")
+        self.generate_peer_config(node_dir, core_yaml_template, gossip_bootstrap)
+        daemon.config_daemon(node_dir, "%s-peer-node$%s" % (self.OrgName, self.Name), "peer node start")
 
     def config_orderer(self, target_dir, mspid, msp_dir, tls_dir, genesis_block):
         pass
@@ -70,7 +80,7 @@ class Organization(BaseConfigModel):
         super().__init__()
         self.update(source)
         if "Nodes" in source:
-            self.Nodes = [Node(**item) for item in source["Nodes"]]
+            self.Nodes = [Node(self, **item) for item in source["Nodes"]]
         self.Dir = None
         self.MspDir = None
 
@@ -92,8 +102,6 @@ class SystemChannel(BaseConfigModel):
         self.update(source)
         if "Organizations" in source:
             self.Organizations = [Organization(**item) for item in source["Organizations"]]
-        if "Orderers" in source:
-            self.Orderers = [Node(**item) for item in source["Orderers"]]
 
     def generate_genesis_block(self, target_dir):
         pass
