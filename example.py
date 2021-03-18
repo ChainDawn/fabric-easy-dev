7af65e564768233d61167bdfd01649570b4076e4
yiwenlong <wlong.yi@gmail.com>
2021-03-19 00:08:42 +0800

    Add an example to show how easy this project to use.

---
+++ example.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2020 Yiwenlong(wlong.yi#gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+from config import Network, env
+
+
+if __name__ == '__main__':
+    network = Network("./example-network.yaml", os.path.join(env.TARGET_DIR, "example"))
+    network.deploy()
+    network.boot()
